f630e34da281097a3f1eca4a34bbe874c4316ba2
Илья Щукин <theilia509@gmail.com>
2024-09-16 18:11:19 +0300

    Add redis

--- llm_functions.py
+++ llm_functions.py
@@ -1,17 +1,22 @@
 import ollama
 from groq import Groq
+import redis
 import httpx
 from httpx_socks import SyncProxyTransport
 import tomli
 import functools
 
-with open("creds.toml", "rb") as f:
-    creds = tomli.load(f)["llm"]
 
-transport = SyncProxyTransport.from_url(creds["proxy"])
+with open("creds.toml", "rb") as f:
+    creds = tomli.load(f)
+
+redis_client = redis.StrictRedis(
+    host="localhost", port=6379, db=0, password=creds["redis"]["password"]
+)
+transport = SyncProxyTransport.from_url(creds["llm"]["proxy"])
 httpx_client = httpx.Client(transport=transport)
 
-@functools.lru_cache(maxsize=128)
+
 def ask_model(host, prompt, max_tokens, seed, temperature=None):
     if host == "local":
         output = ollama.chat(
@@ -27,7 +32,7 @@ def ask_model(host, prompt, max_tokens, seed, temperature=None):
         return output["message"]["content"]
     elif host == "groq":
         client = Groq(
-            api_key=creds["groq_api_key"],
+            api_key=creds["llm"]["groq_api_key"],
             http_client=httpx_client,
         )
 
@@ -55,7 +60,14 @@ def text_contains_topic(banned_topics, text):
 
     print(prompt)
 
-    output = ask_model("groq", prompt, max_tokens=10, seed=42, temperature=0.6)
+    if text := redis_client.get(prompt):
+        output = text.decode("utf-8")
+        print("found output in cache")
+    else:
+        output = ask_model(
+            "groq", prompt, max_tokens=10, seed=42, temperature=0.6
+        )
+        redis_client.set(prompt, output)
 
     print(output)
     print("-------------------")
@@ -72,7 +84,12 @@ def shorten_text(text):
     Сократи данный текст до одного предложения."""
     print(prompt)
 
-    output = ask_model("groq", prompt, max_tokens=100, seed=42)
+    if text := redis_client.get(prompt):
+        output = text.decode("utf-8")
+        print("found output in cache")
+    else:
+        output = ask_model("groq", prompt, max_tokens=100, seed=42)
+        redis_client.set(prompt, output)
 
     print(output)
     print("-------------------")
